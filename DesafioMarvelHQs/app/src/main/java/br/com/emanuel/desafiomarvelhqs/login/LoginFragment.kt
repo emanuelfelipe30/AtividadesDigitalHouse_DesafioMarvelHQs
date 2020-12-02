84076406ce931f3d4269543a23594e28e0417c74
emanuel tavares <emanuelfelipe30@gmail.com>
2020-12-01 21:44:17 -0300

    Implementando a tela de login

--- DesafioMarvelHQs/app/src/main/java/br/com/emanuel/desafiomarvelhqs/login/LoginFragment.kt
+++ DesafioMarvelHQs/app/src/main/java/br/com/emanuel/desafiomarvelhqs/login/LoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import br.com.emanuel.desafiomarvelhqs.R
 
 class LoginFragment : Fragment() {
@@ -15,6 +16,16 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        removeSplashScreenOfBackStack()
+    }
+
+    private fun removeSplashScreenOfBackStack() {
+        Navigation.findNavController(requireView()).popBackStack(
+            R.id.splashFragment, true)
     }
 
 }
